228694fbfdc715bcf0aff3fda8a10630a05ed169
Yuri Britto <ybritto90@gmail.com>
2018-06-09 12:41:54 +0100

    Adding validators, parsers and persisting pagamento into database

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -4,4 +4,35 @@ module.exports = function(app){
 		res.send("ok");
 	});
 
+	app.post("/pagamentos/pagamento", function(req, res){
+
+		var pagamento = req.body;
+		var connection = app.persistencia.connectionFactory();
+		var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+		req.assert("forma_de_pagamento", "Forma de pagamento é obrigatorio").notEmpty();
+		req.assert("valor", "Valor é obrigatorio").notEmpty().isFloat();
+		req.assert("moeda", "Moeda é obrigatorio").notEmpty().len(3,3);
+
+		var errors = req.validationErrors();
+
+		if (errors) {
+			console.log("Erros de validação encontrados");
+			res.status(400).send(errors);
+			return;
+		}
+
+		pagamento.status = "CRIADO";
+    pagamento.data = new Date;
+
+		console.log("Processando pagamento...");
+		pagamentoDao.salva(pagamento, function(exception, result){
+			console.log("Pagamento criado " + result);
+			res.location('/pagamentos/pagamento/' + result.insertId);
+      pagamento.id = result.insertId;
+      res.status(201).json(pagamento);
+		})
+
+	});
+
 }
