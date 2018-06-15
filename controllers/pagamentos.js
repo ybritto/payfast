6a22dbb238550fd2b3b732bde6a2a971ffe8cb37
Britto <ybritto@deloitte.pt>
2018-06-15 11:27:48 +0100

    Adding method for update pagamento

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,5 +1,9 @@
 module.exports = function(app){
 
+	const PAGAMENTO_CRIADO = "CRIADO";
+	const PAGAMENTO_CONFIRMADO = "CONFIRMADO";
+	const PAGAMENTO_CANCELADO = "CANCELADO";
+
 	app.get("/pagamentos", function(req, res){
 		res.send("ok");
 	});
@@ -22,17 +26,81 @@ module.exports = function(app){
 			return;
 		}
 
-		pagamento.status = "CRIADO";
-    pagamento.data = new Date;
+		pagamento.status = PAGAMENTO_CRIADO;
+    	pagamento.data = new Date;
 
 		console.log("Processando pagamento...");
 		pagamentoDao.salva(pagamento, function(exception, result){
 			console.log("Pagamento criado " + result);
 			res.location('/pagamentos/pagamento/' + result.insertId);
-      pagamento.id = result.insertId;
-      res.status(201).json(pagamento);
+      		pagamento.id = result.insertId;
+
+      		var response = {
+      			dados_do_pagamento: pagamento,
+      			links: [
+      				{
+      					href: "http://localhost:3000/pagamentos/pagamento" + pagamento.id;
+      					rel: "confirmar",
+      					method: "PUT"
+      				},
+      				{
+      					href: "http://localhost:3000/pagamentos/pagamento" + pagamento.id;
+      					rel: "cancelar",
+      					method: "DELETE"
+      				}
+      			]
+      		}
+
+      		res.status(201).json(response);
 		})
 
 	});
+
+	app.put("/pagamentos/pagamento/:id", function(req,res){
+
+		var pagamento = {};
+		var id = req.body.id;
+
+		pagamento.id = id;
+		pagamento.status = PAGAMENTO_CONFIRMADO;
+
+
+		var connection = app.persistencia.connectionFactory();
+		var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+		pagamentoDao.atualiza(pagamento, function(erro){
+			if (erro) {
+				res.status(500).send(erro);
+				return;
+			}
+			console.log("Pagamento Atualizado");
+			res.send(pagamento);
+		})
+
+	})
+
+	app.delete("pagamentos/pagamento/:id", function(req, res){
+
+		var pagamento = {};
+		var id = req.body.id;
+
+		pagamento.id = id;
+		pagamento.status = PAGAMENTO_CANCELADO;
+
+		var connection = app.persistencia.connectionFactory();
+		var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+		pagamento.atualiza(pagamento, function(erro){
+			if (erro) {
+				res.status(500).send(erro);
+				return;
+			}
+
+			console.log("Pagamento Canceloado com sucesso");
+			res.status(204).send(pagamento);
+
+		})
+
+	})
 
 }
