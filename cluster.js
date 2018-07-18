de304014b815a496d4c3f0c5333edd7e69ed5d31
Yuri Britto <ybritto90@gmail.com>
2018-07-18 20:09:58 +0100

    implementing cluster

---
+++ cluster.js
@@ -0,0 +1,29 @@
+var cluster = require('cluster');
+var os = require('os');
+
+var cpus = os.cpus();
+console.log('executando thread');
+
+if (cluster.isMaster){
+
+  console.log('thread master');
+
+  cpus.forEach(function(){
+    cluster.fork();
+  })
+
+  cluster.on('listening', function(worker){
+    console.log('cluster iniciado' + worker.process.pid);
+  })
+
+  cluster.on('exit', worker => {
+    console.log('cluster %d desconectado', worker.process.pid);
+    cluster.fork();
+  })
+
+} else {
+
+  console.log('thread slave');
+  require('./index.js');
+
+}
